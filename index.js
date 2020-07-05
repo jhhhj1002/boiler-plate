c6117dbdcefa22802565995da3c0357b6d1e4702
_zz_hyun <jhhhj1002@hanmail.net>
2020-07-06 01:37:00 +0900

    mongoDB url privacy

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = require('./config/key');
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jhhhj1002:Jh742511!!@boilerplate.wkqzc.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology : true, useCreateIndex: true, useFindAndModify : false 
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
