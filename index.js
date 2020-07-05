7ae5974ae823f074a63288ce0e63aa4b2ec66aed
_zz_hyun <jhhhj1002@hanmail.net>
2020-07-05 20:29:06 +0900

    connect mongoDB using mongoose

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://jhhhj1002:Jh742511!!@boilerplate.wkqzc.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology : true, useCreateIndex: true, useFindAndModify : false 
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
