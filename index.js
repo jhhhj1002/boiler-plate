a50435788d804a36b6d6261c70bb90713ec45cf8
_zz_hyun <jhhhj1002@hanmail.net>
2020-07-06 00:57:17 +0900

    add anotation

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const bodyParser = require('body-parser');
 const {User} = require("./models/User");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.post('/register', (req,res) => {
+app.post('/register', (req,res) => { // http://localhost:3000/register 에서 post
     // client에서 회원가입시 필요한 정보들을 가져와서 데이터베이스에 넣어줌
     const user = new User(req.body)
     user.save((err,userInfo) => { // mongeDB 메소드
